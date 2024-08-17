a702e9c7dd0b59dd8a0bfc9117c605a8b7b21c91
adriantwill <atwill@asu.edu>
2024-08-17 14:51:42 -0700

    added oline

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import Card from "./Players";
+import "./index.css";
+import frankRagnow from "./assets/frankragnow.jpeg";
+import kevinZeitler from "./assets/kevinzeitler.jpg";
+import peneiSewell from "./assets/peneisewell.jpg";
+import taylorDecker from "./assets/taylordecker.jpg";
+import grahamgLasgow from "./assets/grahamglasgow.jpg";
 
 export default function App() {
   return (
-    <>
-      <Card></Card>
-    </>
+    <div className="offense">
+      <div className="onLine">
+        <Card name="Decker" number={88} position="Left Tackle" top={"20px"} image={taylorDecker} />
+        <Card name="Glasgow" number={60} position="Left Guard" top={"10px"} image={grahamgLasgow} />
+        <Card name="Ragnow" number={77} position="Center" image={frankRagnow} />
+        <Card name="Zeitler" number={71} position="Right Guard" top={"10px"} image={kevinZeitler} />
+        <Card name="Sewell" number={58} position="Right Tackle" top={"20px"} image={peneiSewell} />
+      </div>
+      <div className="quarterback">
+        <Card name="Sewell" number={58} position="Right Tackle" image={peneiSewell} />
+      </div>
+    </div>
   );
 }
--- src/Players.tsx
+++ src/Players.tsx
@@ -1,10 +1,24 @@
-export default function Card() {
+type PlayerProp = {
+  name: string;
+  number: number;
+  position: string; // Define the style prop type
+  top?: string;
+  image: string;
+};
+
+function Card(props: PlayerProp) {
+  const handleClick = () => console.log("test");
+  const divStyle = {
+    top: props.top,
+  };
   return (
-    <div className="card">
-      <img></img>
-      <h2>Name</h2>
-      <h1>Number</h1>
-      <h3>Position</h3>
+    <div style={divStyle} className="card" onClick={handleClick}>
+      <img className="picture" src={props.image}></img>
+      <div className="name">{props.name}</div>
+      <div className="number">{props.number}</div>
+      <div className="position">{props.position}</div>
     </div>
   );
 }
+
+export default Card;
