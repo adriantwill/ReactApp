b5eb2378bd8e16725b2f44075991012f7fa7f18e
adriantwill <atwill@asu.edu>
2024-09-07 18:47:29 -0700

    migrate css to tailwind

--- src/components/Players.tsx
+++ src/components/Players.tsx
@@ -12,8 +12,15 @@ function Card(props: PlayerProp) {
     top: props.top,
   };
   return (
-    <div style={divStyle} className="card" onClick={handleClick}>
-      <img className="picture" src={props.image}></img>
+    <div
+      style={divStyle}
+      className="border shadow-[5px_5px_5px_hsla(0,0%,0%,0.1)] text-center max-w-[120px] min-w-[120px] max-h-[360px] inline-block bg-[#0076b6] relative m-[15px] p-[5px] rounded-[10px] border-solid border-[black] hover:cursor-pointer hover:scale-105"
+      onClick={handleClick}
+    >
+      <img
+        className=" max-w-[95%] shadow-[5px_5px_5px_hsla(0,0%,0%,0.1)] bg-[#b0b7bc] rounded-[10px] border-[0.5px] border-solid border-[black]˝"
+        src={props.image}
+      ></img>
       <div className="name">{props.name}</div>
       <div className="number">{props.number}</div>
       <div className="position">{props.position}</div>
