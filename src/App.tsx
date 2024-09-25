e983f54897048b7e1d7f8aae7731ff7ddf995a95
adriantwill <atwill@asu.edu>
2024-09-25 13:12:01 -0700

    added title

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Card from "./components/Players";
+import Title from "./components/Title";
 import "./index.css";
 import frankRagnow from "./assets/frankheadshot.png";
 import kevinZeitler from "./assets/kevinheadshot.png";
@@ -9,21 +10,28 @@ import jaredGoff from "./assets/goffheadshot.png";
 
 function App() {
   return (
-    <div className="offense">
-      <div className="flex justify-center items-center">
-        <Card name="Decker" number={88} position="Left Tackle" image={taylorDecker} tailwind="top-6" />
-        <Card name="Glasgow" number={60} position="Left Guard" image={grahamgLasgow} tailwind="top-3" />
-        <Card name="Ragnow" number={77} position="Center" image={frankRagnow} />
-        <Card name="Zeitler" number={71} position="Right Guard" image={kevinZeitler} tailwind="top-3" />
-        <Card name="Sewell" number={58} position="Right Tackle" image={peneiSewell} tailwind="top-6" />
+    <>
+      <Title />
+      <div>
+        <div className="flex justify-center items-center">
+          <Card name="Decker" number={88} position="Left Tackle" image={taylorDecker} tailwind="top-6" />
+          <Card name="Decker" number={88} position="Left Tackle" image={taylorDecker} tailwind="top-8" />
+          <Card name="Decker" number={88} position="Left Tackle" image={taylorDecker} tailwind="top-6" />
+          <Card name="Glasgow" number={60} position="Left Guard" image={grahamgLasgow} tailwind="top-3" />
+          <Card name="Ragnow" number={77} position="Center" image={frankRagnow} />
+          <Card name="Zeitler" number={71} position="Right Guard" image={kevinZeitler} tailwind="top-3" />
+          <Card name="Sewell" number={58} position="Right Tackle" image={peneiSewell} tailwind="top-6" />
+          <Card name="Sewell" number={58} position="Right Tackle" image={peneiSewell} tailwind="top-9" />
+          <Card name="Sewell" number={58} position="Right Tackle" image={peneiSewell} tailwind="top-6" />
+        </div>
+        <div className="flex justify-center items-center">
+          <Card name="Goff" number={16} position="Quarterback" image={jaredGoff} />
+        </div>
+        <div className="flex justify-center items-center">
+          <Card name="Goff" number={16} position="Quarterback" image={jaredGoff} tailwind="left-30" />
+        </div>
       </div>
-      <div className="flex justify-center items-center">
-        <Card name="Goff" number={16} position="Quarterback" image={jaredGoff} />
-      </div>
-      <div className="flex justify-center items-center">
-        <Card name="Goff" number={16} position="Quarterback" image={jaredGoff} tailwind="left-30" />
-      </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Title.tsx
@@ -0,0 +1,4 @@
+function Title() {
+  return <h1 className="bg-blue-500 text-white font-bold text-3xl px-8 py-4 rounded-lg">My Title</h1>;
+}
+export default Title;
