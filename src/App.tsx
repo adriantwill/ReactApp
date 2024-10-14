0eb80bf72d576aaec5017532650a158caf9bbe76
adriantwill <atwill@asu.edu>
2024-10-13 20:46:08 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,38 @@
+import "./index.css";
 import Card from "./components/Players";
 import Title from "./components/Title";
-import "./index.css";
 import frankRagnow from "./assets/frankheadshot.png";
 import kevinZeitler from "./assets/kevinheadshot.png";
 import peneiSewell from "./assets/peneiheadshot.png";
 import taylorDecker from "./assets/taylorheadshot.png";
 import grahamgLasgow from "./assets/grahamheadshot.png";
 import jaredGoff from "./assets/goffheadshot.png";
+import samlaporta from "./assets/samlaporta.png";
+import amonrastbrown from "./assets/amonrastbrown.png";
+import jamesonwilliams from "./assets/jamesonwilliams.png";
+import jahmyrgibbs from "./assets/jahmyrgibbs.png";
+import timpatrick from "./assets/timpatrick.png";
 
 function App() {
   return (
-    <div className="w-11/12 h-5/6 m-auto">
+    <div className=" m-auto">
       <Title teamName="Detroit Lions" />
-      <div className="bg-[url('./assets/background.jpg')] bg-no-repeat bg-cover overflow-hidden">
+      <div className="bg-[url('./assets/background.jpg')] bg-cover ">
         <div className="flex justify-center">
-          <Card
-            name="Decker"
-            number={88}
-            position="LT"
-            image={taylorDecker}
-            tailwind="top-6 mr-auto left-6"
-          />
-          <Card name="Decker" number={88} position="LT" image={taylorDecker} tailwind="top-12 right-14" />
+          <Card name="Williams" number={9} position="WR" image={jamesonwilliams} tailwind="top-6 mr-auto" />
+          <Card name="St Brown" number={14} position="WR" image={amonrastbrown} tailwind="top-10 right-12" />
           <Card name="Decker" number={88} position="LT" image={taylorDecker} tailwind="top-6" />
           <Card name="Glasgow" number={60} position="LG" image={grahamgLasgow} tailwind="top-3" />
           <Card name="Ragnow" number={77} position="C" image={frankRagnow} />
           <Card name="Zeitler" number={71} position="RG" image={kevinZeitler} tailwind="top-3" />
           <Card name="Sewell" number={58} position="RT" image={peneiSewell} tailwind="top-6" />
-          <Card name="Sewell" number={58} position="RT" image={peneiSewell} tailwind="top-16" />
-          <Card
-            name="Sewell"
-            number={58}
-            position="RT"
-            image={peneiSewell}
-            tailwind="top-6 ml-auto right-6"
-          />
+          <Card name="Laporta" number={87} position="TE" image={samlaporta} tailwind="top-16" />
+          <Card name="Patrick" number={81} position="WR" image={timpatrick} tailwind="top-6 ml-auto" />
         </div>
         <div className="flex justify-center">
           <Card name="Goff" number={16} position="QB" image={jaredGoff} tailwind="mb-14" />
-          <div className="flex absolute ml-64 mt-10">
-            <Card name="Goff" number={16} position="RB" image={jaredGoff} />
+          <div className="flex absolute ml-64 mt-8">
+            <Card name="Gibbs" number={26} position="RB" image={jahmyrgibbs} />
           </div>
         </div>
       </div>
--- src/components/Players.tsx
+++ src/components/Players.tsx
@@ -10,15 +10,15 @@ function Card(props: PlayerProp) {
   const handleClick = () => console.log("test");
   return (
     <div
-      className={`border shadow-[5px_5px_5px_hsla(0,0%,0%,0.1)] text-center w-24 h-44 inline bg-[#0076b6] relative m-4 p-[5px] rounded-[10px] border-solid border-[black] hover:cursor-pointer hover:scale-105 mt-14 ${props.tailwind}`}
+      className={`border-black border drop-shadow-xl text-center w-24 h-44 inline bg-[#0076b6] relative m-4 p-1.5 rounded-xl border-solid hover:cursor-pointer hover:scale-110 transition duration-500 ease-in-out mt-20 ${props.tailwind}`}
       onClick={handleClick}
     >
       <img
-        className=" shadow-[5px_5px_5px_hsla(0,0%,0%,0.1)] bg-[#b0b7bc] rounded-[10px] border-[0.5px] border-solid border-[black]˝"
+        className=" drop-shadow-xl bg-[#b0b7bc] rounded-xl border-[0.5px] border-solid border-black"
         src={props.image}
       ></img>
-      <div className="font-normal text-lg text-white mb-[5px]">{props.name}</div>
-      <div className="text-3xl font-medium text-[white] mb-2.5">{props.number}</div>
+      <div className="font-normal text-lg text-white mb-1.5">{props.name}</div>
+      <div className="text-3xl font-medium text-white mb-2.5">{props.number}</div>
       <div className="text-white text-lg font-normal">{props.position}</div>
     </div>
   );
