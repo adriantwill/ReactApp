609c4a75e96053d87d78aecbb614817370a76fcc
adriantwill <atwill@asu.edu>
2025-01-28 14:28:08 -0700

    update

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"log"
 	"os"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -20,6 +22,7 @@ type Todo struct {
 var collection *mongo.Collection
 
 func main() {
+	fmt.Println("Hello, World!")
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -30,12 +33,57 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Hello, World!")
 	err = client.Ping(context.Background(), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer client.Disconnect(context.Background())
 	fmt.Println("Connected to MongoDB!")
+	collection = client.Database("goland_db").Collection("todos")
+	app := fiber.New()
+	app.Get("/api/todos", getTodos)
+	app.Post("/api/todos", createTodos)
+	// app.Patch("/api/todos/:id", updateTodo)
+	// app.Delete("/api/todos/:id", deleteTodo)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4000"
+	}
+	log.Fatal(app.Listen(":" + port))
+}
+
+func getTodos(c *fiber.Ctx) error {
+	var todos []Todo
+	cursor,err := collection.Find(context.Background(), bson.M{})
+	if err != nil{
+		return err
+	}
+	defer cursor.Close(context.Background())
+	for cursor.Next(context.Background()){
+		var todo Todo
+		if err := cursor.Decode(&todo); err != nil {
+			return err
+		}
+		todos = append(todos, todo)
+	}
+	return c.JSON(todos)
+}
+
+func createTodos(c *fiber.Ctx) error {
+	todo := new(Todo)
+	if err := c.BodyParser(todo); err != nil {
+		return err
+	}
+	if todo.Body == "" {
+		return c.Status(400).JSON(fiber.Map{"error": "Body is required"})
+	}
+	insertResult,err := collection.InsertOne(context.Background(),todo)
+	if err != nil {
+		return err
+	}
+	
+}
+
 // 	app := fiber.New()
 // 	todos := []Todo{}
 // 	app.Get("/api/todos", func(c *fiber.Ctx) error {
@@ -74,4 +122,3 @@ func main() {
 // 		return c.Status(200).JSON(fiber.Map{"success": true})
 // 	})
 // 	log.Fatal(app.Listen(":4000"))
-}
